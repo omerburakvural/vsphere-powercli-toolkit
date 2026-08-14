b1e70944501a39aa93b9c5a420442e8ff45db242
Omer Burak Vural <omerburakvural@gmail.com>
2026-08-14 21:31:47 +0300

    Create Get-VIRole.ps1

---
+++ Get-VIRole.ps1
@@ -0,0 +1,25 @@
+# Script      : Get-VIRole.ps1
+# Author      : Burak Vural
+# Description : Exports vSphere role privileges as string.
+
+#Get-Module -ListAvailable
+#Import-Module viToolkitExtensions -PassThru
+
+$Role = Get-TkeRoles | Where { $_.Name -eq "NetApp VSC Optimization" }
+$StringPrivs = ""
+$Count = 1
+
+foreach ($strPriv in $Role.Privilege) {
+	if ($Count -eq $Role.Privilege.Count) {
+		$StringPrivs = $StringPrivs + '"' + $strPriv + '"'
+	} 
+	else {
+		$StringPrivs = $StringPrivs + '"' + $strPriv + '"' + ',' 
+	}
+	$Count++
+}
+
+Write-Output $StringPrivs | Out-File D:\VIRole_NetappOptimization.txt
+
+#New-TkeRole -name "NetApp VSC Optimization" -privIds "VirtualMachine.Interact.PowerOn","VirtualMachine.Interact.PowerOff"
+#Set-TkeRole -name $Role.Name -privIds "VirtualMachine.Interact.PowerOn","VirtualMachine.Interact.PowerOff"
